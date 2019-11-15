7a15656fa1a5c8efc4f5d556f0dfda88f1980482
kais.bitar@yahoo.com <kais.bitar@yahoo.com>
2019-11-15 12:21:32 -0700

    fixing a string in sanatizer

--- app/Sanatizer.php
+++ app/Sanatizer.php
@@ -119,6 +119,7 @@ class Sanatizer extends Model
         $cleanedSura = str_replace('الذى', 'الذي', $cleanedSura);  
         $cleanedSura = str_replace('ابرهيملابيه', 'ابرهيم لابيه', $cleanedSura);  
         $cleanedSura = str_replace('بلقاىءربهم', 'بلقاء ربهم', $cleanedSura); 
+        $cleanedSura = str_replace('مصلىوعهدنا', 'مصلى وعهدنا', $cleanedSura); 
         $cleanedSura = str_replace('يحى', 'يحي', $cleanedSura); 
         $cleanedSura = mb_substr($cleanedSura, 0, -1);
         $cleanedSura = str_replace('عمران,', '', $cleanedSura); 
